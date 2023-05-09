301470949298716c13603298c7158558b702869e
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-05-09 19:45:43 -0400

    Need to finish

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+// imports express package
 
 const hbs = require('hbs');
+// imports hbs
 const path = require('path');
+// imports path which is the thing that allows us to tell us to tell hbs where to find 
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
+// this line initializes 
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
@@ -30,14 +34,48 @@ app.get('/beers', (req, res) => {
   .catch(error => console.log(error));
 })
 
+// Using fetch
+/*
+app.get('/beers', (req, res, next)=> {
+  fetch('http://api.punkapi.com/v2/beers')
+  .then((response)=> {
+    response.json()
+    .then((jsonResponse)=> {
+      res.render('beers', {beers: jsonResponse});
+    })
+  })
+  .catch((err)=> {
+    console.log(err);
+  })
+})
+*/
+
 // render random beer
 app.get('/random-beer', (req, res) => {
   punkAPI.getRandom()
   .then(beer => {
-    console.log(beer)
-    res.render('random-beer', { beer })
+    console.log(beer);
+    const theBeer = beer[0];
+    res.render('random-beer', {theBeer})
   })
   .catch(error => console.log(error));
 })
+
+// Using fetch
+/*
+app.get('/random-beer', (req, res, next)=> {
+  fetch('http://api.punkapi.com/v2/beers/random')
+  .then((response)=> {
+    response.json()
+    .then((jsonResponse)=> {
+      const theBeer = jsonResponse
+      res.render('beers', {beers: jsonResponse});
+    })
+  })
+  .catch((err)=> {
+    console.log(err);
+  })
+})
+*/
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
